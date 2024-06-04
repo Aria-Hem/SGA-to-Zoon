d221d4de711568d0581a1cffaea8f1ef480f5959
Aria Hemmatian <AriaHemmatian81@gmail.com>
2024-06-04 12:16:37 +0330

    Added the ellipse drawing function and cleaned up the code

--- main.py
+++ main.py
@@ -5,31 +5,69 @@ from astropy.utils.data import get_pkg_data_filename
 from astropy.visualization import astropy_mpl_style
 import urllib
 import pathlib
+from PIL import Image
 path = str(pathlib.Path(__file__).parent.resolve())
 
-#Opening the FITS file
-image_file = get_pkg_data_filename(r'sga2020-2masx.fits')
 
-#data_list = fits.getdata(image_file, ext=0)
-fits.info(image_file)
-fitsfile = fits.open(image_file)
-hdu = 1
-table = fitsfile[hdu].data
+def data_coordinate_plot(table): #Ploting RA and DEC of the Galaxies
+    RA = table['ra']
+    DEC = table.field('dec')
+    plt.scatter(RA,DEC, marker = '.')
+    plt.title('Coordinate of the Galaxies in the FITS file')
+    plt.xlabel('RA (deg)')
+    plt.ylabel('DEC (deg)')
+    plt.show()
 
-#Ploting RA and DEC of the Galaxies
-RA = table['ra']
-DEC = table.field('dec')
-plt.scatter(RA,DEC, marker = '.')
-plt.title('Coordinate of the Galaxies in the FITS file')
-plt.xlabel('RA (deg)')
-plt.ylabel('DEC (deg)')
-#plt.show()
+def openFITS(path , hdu = 1): #Opening the FITS file
+    image_file = get_pkg_data_filename(path)
+    fits.info(image_file)
+    fitsfile = fits.open(image_file)
+    return fitsfile[hdu].data
 
-#Downloading a cutout of each galaxy from www.legacysurvey.org
-for i in table:
-    url = 'https://www.legacysurvey.org/viewer/cutout.jpg?ra=' +str(i[1])+ '&dec=' +str(i[2])+ '&layer=ls-dr9&pixscale=0.1&size='+str(int(600*i['d26']*2))
-    print(url)
-    file_name = str(i[6]) + ".jpg"
-    file_path = path + r"\output" + "\\"
-    print(file_path+file_name)
-    urllib.request.urlretrieve(url, file_path+file_name)
+def draw_ellipse_on_png(im, x0, y0, ba, pa, major_axis_diameter_arcsec,
+                        pixscale, color='#3388ff', linewidth=3):  #Draws an ellipse on the galaxy (from https://github.com/moustakas/legacyhalos/)
+
+    from PIL import Image, ImageDraw, ImageFont
+
+    Image.MAX_IMAGE_PIXELS = None
+    
+    minor_axis_diameter_arcsec = major_axis_diameter_arcsec * ba
+
+    overlay_height = int(major_axis_diameter_arcsec / pixscale)
+    overlay_width = int(minor_axis_diameter_arcsec / pixscale)
+    overlay = Image.new('RGBA', (overlay_width, overlay_height))
+
+    draw = ImageDraw.ImageDraw(overlay)
+    box_corners = (0, 0, overlay_width, overlay_height)
+    draw.ellipse(box_corners, fill=None, outline=color, width=linewidth)
+
+    rotated = overlay.rotate(pa, expand=True)
+    rotated_width, rotated_height = rotated.size
+    paste_shift_x = int(x0 - rotated_width / 2)
+    paste_shift_y = int(y0 - rotated_height / 2)
+    im.paste(rotated, (paste_shift_x, paste_shift_y), rotated)
+
+def galaxy_image_exporter(table,ellipse = True, show = True, save = False):  #Downloading a cutout of each galaxy from www.legacysurvey.org
+    for glx in table:
+        pixscale = 0.1
+        size = int(120*glx['d26']/pixscale)
+        url = 'https://www.legacysurvey.org/viewer/cutout.jpg?ra=' +str(glx['ra'])+ '&dec=' +str(glx['dec'])+ '&layer=ls-dr9&pixscale='+str(pixscale)+'&size='+str(size)
+
+        img = Image.open(urllib.request.urlopen(url))
+        if ellipse:
+            draw_ellipse_on_png(img,size/2-0.5,size/2-0.5,glx['ba'],glx['pa'],glx['d26']*60,pixscale)
+        if show:
+            plt.imshow(img)
+            plt.show()
+        if save:
+            file_name = str(glx['galaxy']) + ".jpg"
+            file_path = path + r"\output" + "\\"
+            img.save(file_path+file_name)
+            urllib.request.urlretrieve(url, file_path+file_name)
+
+
+
+table = openFITS(r'sga2020-2masx.fits')
+#data_coordinate_plot(table)
+galaxy_image_exporter(table)
+
