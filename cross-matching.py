289f665d6e627a6155db274a84e3c68b31e13bed
Aria Hemmatian <AriaHemmatian81@gmail.com>
2024-06-27 19:55:28 +0330

    Fixed some issues and added the feature to save the coordinatesof un-matched objects in a file.

--- cross-matching.py
+++ cross-matching.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from astropy.utils.data import get_pkg_data_filename
 from astropy.visualization import astropy_mpl_style
+from astropy.table import vstack, Table
 import urllib
 import pathlib
 from PIL import Image, ImageDraw, ImageFont
@@ -33,43 +34,125 @@ def openFITS(path , hdu = 1): #Opening the FITS file
     return fitsfile[hdu].data
 
 def opentbl(path):
-    return pandas.read_table(mainpath + path, comment='#', delim_whitespace=True)
+    return pandas.read_table(mainpath + path, comment='#',delim_whitespace=True)
     
+def area_check(table,RA,DEC,Riso):
+    N = len(table[DEC]) - 2
+    in_area = np.zeros(N)
+    sizes = np.array([])
+    with alive_bar(N) as bar:
+        for i in range(N):
+            ra = float(table[RA][i+2]) * np.pi/180
+            dec = float(table[DEC][i+2]) * np.pi/180
+            center = 22.5 * np.pi/180
+            radius = 10 * np.pi/180
+            dist = np.arccos(np.sin(dec)*np.sin(center)+np.cos(dec)*np.cos(center)*np.cos(np.pi+ra))
+            if dist<radius:
+                in_area[i]=1
+                sizes = np.append(sizes,float(table[Riso][i+2]))
+            bar()
+    return in_area , sizes
+
+def area_size_check(table,RA,DEC,Riso,upper,lower):
+    N = len(table[DEC]) - 2
+    in_area = np.zeros(N)
+    sizes = np.array([])
+    with alive_bar(N) as bar:
+        for i in range(N):
+            ra = float(table[RA][i+2]) * np.pi/180
+            dec = float(table[DEC][i+2]) * np.pi/180
+            center = 22.5 * np.pi/180
+            radius = 10 * np.pi/180
+            dist = np.arccos(np.sin(dec)*np.sin(center)+np.cos(dec)*np.cos(center)*np.cos(np.pi+ra))
+            if dist<radius and float(table[Riso][i+2]) <= upper and float(table[Riso][i+2]) >= lower:
+                in_area[i]=1
+            bar()
+    return in_area
+
+
+def match_for_size(table,RA,DEC,ref_table,in_area):
+    margin = 1/360
+    matching = Table({'ra':[],'dec':[]},names=('ra','dec'))
+    unmatching =  Table({'ra':[],'dec':[]},names=('ra','dec'))
+    N = len(table[DEC]) - 2
+    counter = 0
+    with alive_bar(N) as bar:
+        for i in range(N):
+            if in_area[i]:
+                c = 0
+                ra = float(table[RA][i+2])
+                dec = float(table[DEC][i+2])
+                temp = Table({'ra':[ra],'dec':[dec]},names=('ra','dec'))
+                for glx in ref_table:
+                    if np.abs(glx['ra']-ra) <= margin and np.abs(glx['dec']-dec) <= margin :
+                        counter += 1
+                        c = 1
+                        matching = vstack([matching,temp])
+                        break
+                if c==0:
+                    unmatching = vstack([unmatching,temp])
+            bar()
+    return matching, unmatching ,counter
+
+
+
+def cross_matching(table, RA, DEC, ref_table, in_area):
+    margin = 1/360
+    N = len(table[DEC]) - 2
+    counter = 0
+    sizes = np.array([])
+    with alive_bar(N) as bar:
+        for i in range(N):
+            if in_area[i]:
+                ra = float(table[RA][i+2])
+                dec = float(table[DEC][i+2])
+                temp = Table({'ra':[ra],'dec':[dec]},names=('ra','dec'))
+                for glx in ref_table:
+                    if np.abs(glx['ra']-ra) <= margin and np.abs(glx['dec']-dec) <= margin :
+                        counter += 1
+                        sizes = np.append(sizes,float(table[Riso][i+2]))
+                        break
+            bar()
+    return counter, sizes
 
 ref_table = openFITS(r'sga-query.fits')
 RA = '\\'
 DEC = 'WXSC'
+Riso = '(jarrett'
 table = opentbl(r'\WXSC_Riso_1arcmin_10Jun2024.tbl')
 
-N = len(table[DEC]) - 2
-#print( type(table[DEC][2]) , len(table[DEC]) )
-in_area = np.zeros(N)
+in_area = area_size_check(table,RA,DEC,Riso,10000,0)
 
-with alive_bar(N) as bar:
-    for i in range(N):
-        ra = float(table[RA][i+2]) * np.pi/180
-        dec = float(table[DEC][i+2]) * np.pi/180
-        center = 22.5 * np.pi/180
-        radius = 10 * np.pi/180
-        dist = np.arccos(np.cos(dec)*np.cos(center)+np.sin(dec)*np.sin(center)*np.cos(np.pi+ra))
-        if dist<radius :
-            in_area[i]=1
-        bar()
+#ra = np.array([])
+#dec = np.array([])
+#for i in range(len(table[DEC])-2):
+#    if in_area[i] == 1:
+#        ra = np.append(ra,float(table[RA][i+2]))
+#        dec = np.append(dec,float(table[DEC][i+2]))
+#plt.scatter(ra,dec, marker = '.')
+#plt.title('Coordinate of the Galaxies in the FITS file')
+#plt.xlabel('RA (deg)')
+#plt.ylabel('DEC (deg)')
+#plt.show()
+
+matching, unmatching , counter = match_for_size(table,RA,DEC,ref_table,in_area)
+matching.write('matching.fits', format='fits',overwrite=True)
+unmatching.write('unmatching.fits', format='fits',overwrite=True)
+
+data_coordinate_plot(matching)
+data_coordinate_plot(unmatching)
+
+in_area , all_sizes = area_check(table,RA,DEC,Riso)
+counter, sizes = cross_matching(table,RA,DEC,ref_table, in_area)
 
 total_in_area = np.sum(in_area)
-counter = 0
-margin = 1/360
-
-with alive_bar(N) as bar:
-    for i in range(N):
-        if in_area[i]:
-            ra = float(table[RA][i+2])
-            dec = float(table[DEC][i+2])
-            for glx in ref_table:
-                if np.abs(glx['ra']-ra) <= margin and np.abs(glx['dec']-dec) <= margin :
-                    counter += 1
-                    break
-        bar()   
-
 print(counter,total_in_area,100 * counter/total_in_area)
-#data_coordinate_plot(table)
+
+bins = np.linspace(30, 600, 50)
+
+plt.hist(all_sizes, bins, alpha=0.5, label='WXSC')
+plt.hist(sizes, bins, alpha=0.5, label='SGA')
+plt.legend(loc='upper right')
+plt.title('Histogram of Riso')
+plt.xlabel('Riso (arcsec)')
+plt.show()
